c9b573d28b9725a87a5d498172f084372a8f9750
Elster07 <136040147+Elster07@users.noreply.github.com>
2023-06-09 05:09:18 +0200

    Greetings added

--- DataManagementEx1.py
+++ DataManagementEx1.py
@@ -9,6 +9,10 @@ b = random.randint(1,6)
 #Total value
 t = a+b
 
+# Greeting
+name = input("Hello, What's your name?")
+print("Hello " + name +"!")
+
 print ("Rolling dice...")
 print ("Die 1:", a)
 print ("Die 2:", b)
